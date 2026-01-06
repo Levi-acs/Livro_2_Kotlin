40eb062b2e518a6ab1975edcb4204a7a7e681f12
levi-acs <levi.acsinformatica@gmail.com>
2026-01-06 14:39:42 -0300

    Subindo class e layout main

--- Lista de Compras/app/src/main/java/livrokotlin/com/lista_de_compas/MainActivity.kt
+++ Lista de Compras/app/src/main/java/livrokotlin/com/lista_de_compas/MainActivity.kt
@@ -2,14 +2,32 @@ package livrokotlin.com.lista_de_compas
 
 import android.os.Bundle
 import android.widget.ArrayAdapter
-import androidx.activity.ComponentActivity
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+
+import androidx.appcompat.app.AppCompatActivity
 
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val adapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1)
+        val ListViewProdutos = findViewById<ListView>(R.id.list_view_produtos)
+
+        val btn_inserir = findViewById<Button>(R.id.btn_inserir)
+
+        val txt_produto = findViewById<EditText>(R.id.txt_produto)
+
+        val produtosAdapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1)
+
+        ListViewProdutos.adapter = produtosAdapter
+
+       btn_inserir.setOnClickListener {
+           val produto = txt_produto.text.toString()
+           produtosAdapter.add(produto)
+       }
     }
 }
