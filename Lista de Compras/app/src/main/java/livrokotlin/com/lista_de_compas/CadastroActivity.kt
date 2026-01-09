6e9eb90a385b96eb99dbd5fb4799e7d98f0af06c
ZNitr0 <levidonascimento2410@gmail.com>
2026-01-08 23:08:07 -0300

    implementando cadastro de produtos

--- Lista de Compras/app/src/main/java/livrokotlin/com/lista_de_compas/CadastroActivity.kt
+++ Lista de Compras/app/src/main/java/livrokotlin/com/lista_de_compas/CadastroActivity.kt
@@ -3,6 +3,8 @@ package livrokotlin.com.lista_de_compas
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ListView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -14,19 +16,38 @@ class CadastroActivity : AppCompatActivity() {
         val btn_inserir = findViewById<Button>(R.id.btn_inserir)
 
         val txt_produto = findViewById<EditText>(R.id.txt_produto)
+        val txt_qtd = findViewById<EditText>(R.id.txt_quantidade)
+        val txt_valor = findViewById<EditText>(R.id.txt_valor)
 
         btn_inserir.setOnClickListener {
             val produto = txt_produto.text.toString()
+            val qtd = txt_qtd.text.toString()
+            val valor = txt_valor.text.toString()
 
-            if (produto.isNotEmpty()) {
-                //adicionando produto
+            if (produto.isNotEmpty() && qtd.isNotEmpty() && valor.isNotEmpty()){
+                val prod = Produto(produto,qtd.toInt(),valor.toDouble())
 
-                //limpando o campo de pesquisa
+                produtosGlobal.add(prod)
+
+                println("✅ Produto adicionado!")
+                println("📦 Nome: ${prod.nome}")
+                println("🔢 Quantidade: ${prod.quantidade}")
+                println("💰 Valor: R$ ${prod.valor}")
+                println("📊 Total de produtos na lista: ${produtosGlobal.size}")
+                println("-----------------------------------")
+
                 txt_produto.text.clear()
-            } else {
-                // barrando caso usuario não digite nada no campo
-                txt_produto.error = "Preencha um valor"
+                txt_qtd.text.clear()
+                txt_valor.text.clear()
+
+            }else{
+                txt_produto.error = if (txt_produto.text.isEmpty())"Preencha o nome do produto" else null
+                txt_qtd.error = if (txt_qtd.text.isEmpty()) "Preencha a quantidade" else null
+                txt_valor.error = if (txt_valor.text.isEmpty()) "Preencha o valor" else null
             }
+
+
+
         }
 
     }
